c78f9b9bc64cfba9b3ddd8662f99d1df95095e4e
João <joao.vitor012020@gmail.com>
2024-06-23 18:55:01 -0300

    feat: create Titlebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Titlebar } from './components/Titlebar';
+
 function App() {
   return (
-    <section>
+    <main>
+      <Titlebar />
+
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </section>
+    </main>
   );
 }
 

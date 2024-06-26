a4107e87078e2d3abbd93e8034861d15d1eaee61
João <joao.vitor012020@gmail.com>
2024-06-25 21:27:22 -0300

    feat: render PdfViewer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+import PdfViewer from './components/PdfViewer';
 import { Titlebar } from './components/Titlebar';
 
 function App() {
   return (
-    <main className='bg-black h-screen rounded-lg border border-gray-50/20'>
+    <main className='bg-black pt-9 h-screen rounded-xl border border-gray-50/20 overflow-hidden'>
       <Titlebar />
 
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <PdfViewer />
     </main>
   );
 }

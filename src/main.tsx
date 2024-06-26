d07b6a8704c927eb5d55649ffaa794267f95ae07
João <joao.vitor012020@gmail.com>
2024-06-26 18:44:13 -0300

    feat: add geist provider on top of app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GeistProvider } from '@geist-ui/core';
 import './styles.css';
 import './viewer.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GeistProvider themeType={'dark'}>
+      <App />
+    </GeistProvider>
   </React.StrictMode>,
 );

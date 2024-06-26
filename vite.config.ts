90fa828da8c80494849edaac6a621440e13fadfd
João <joao.vitor012020@gmail.com>
2024-06-25 21:27:01 -0300

    chore: add config for pdfjs-dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from 'vite';
+import path from 'node:path';
 import react from "@vitejs/plugin-react";
+import { createRequire } from 'node:module';
+
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const require = createRequire(import.meta.url);
+const cMapsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'),
+);
+const standardFontsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'standard_fonts'),
+);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: cMapsDir, dest: '' },
+        { src: standardFontsDir, dest: '' },
+      ],
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //

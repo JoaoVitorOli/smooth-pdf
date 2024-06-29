45e003e3b71ce99eb898d27eafbb38cc85d093bf
João <joao.vitor012020@gmail.com>
2024-06-29 18:29:50 -0300

    fix: add @ts-expect-error

--- src/components/PagesCount.tsx
+++ src/components/PagesCount.tsx
@@ -5,6 +5,8 @@ export function PagesCount() {
   return (
     <div className='absolute z-30 left-[50%] bottom-5 -translate-x-[50%]'>
       <Popover
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
         content={Content}
         placement='top'
         trigger='hover'
@@ -22,18 +24,20 @@ export function PagesCount() {
   );
 }
 
-const Content = () => (
-  <div className='flex items-center gap-4 px-3'>
-    <p>Page:</p>
-    <Input
-      w={3}
-      placeholder="1"
-      type='secondary'
-      scale={2 / 3}
-      crossOrigin={undefined}
-      onPointerEnterCapture={undefined}
-      onPointerLeaveCapture={undefined}
-    />
-  </div>
-);
+function Content() {
+  return (
+    <div className='flex items-center gap-4 px-3'>
+      <p>Page:</p>
+      <Input
+        w={3}
+        placeholder="1"
+        type='secondary'
+        scale={2 / 3}
+        crossOrigin={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      />
+    </div>
+  );
+}
 

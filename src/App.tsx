e8b6ed3c1f4547d9329c12235e3c1948ac7cfc12
João <joao.vitor012020@gmail.com>
2024-06-29 18:26:57 -0300

    refactor: add lazyload for PdfViewer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import Loading from '@geist-ui/core/esm/loading';
 import { ButtonsNextPrev } from './components/ButtonsNextPrev';
 import { PagesCount } from './components/PagesCount';
-import PdfViewer from './components/PdfViewer';
 import { Titlebar } from './components/Titlebar';
+import { Suspense, lazy } from 'react';
+const PdfViewer = lazy(() => import('./components/PdfViewer'));
 
 function App() {
+
   return (
     <main className='bg-black pt-9 h-screen rounded-xl border border-gray-50/20 overflow-hidden'>
       <Titlebar />
 
-      <PdfViewer />
+      <Suspense fallback={<Loading scale={2} />}>
+        <PdfViewer />
+      </Suspense>
 
       <PagesCount />
       <ButtonsNextPrev />
